7de68ce780b342f69aab418e39719dc29ccb1a41
zhangdaren3389 <375890534@qq.com>
2019-05-21 19:35:38 +0800

    解决月星期显示不对的问题，适配最新自定义组件编译模式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,69 +1,67 @@
 <template>
-	<view class="content">
-		<calendar @change="change"></calendar>
-	</view>
+	<view class="content"><calendar @change="change"></calendar></view>
 </template>
 
 <script>
-	import calendar from '../../components/date-picker/date-picker.vue';
+import calendar from '../../components/date-picker/date-picker.vue';
 
-	export default {
-		data() {
-			return {}
-		},
-		components: {
-			calendar
-		},
-		onLoad() {
+export default {
+	data() {
+		return {
+			beginDate: '2019-05-06',
+			endDate: '2019-05-07'
+		};
+	},
+	components: {
+		calendar
+	},
+	onLoad() {},
+	methods: {
+		change({ choiceDate, dayCount }) {
+			//参数解释
+			//1.choiceDate 时间区间（开始时间和结束时间）
+			//2.dayCount 共多少晚
 
-		},
-		methods: {
-			change({choiceDate, dayCount})
-			{
-				//参数解释
-				//1.choiceDate 时间区间（开始时间和结束时间）
-				//2.dayCount 共多少晚
-				
-				console.log(choiceDate, dayCount)
-				console.log("入住从 "+ choiceDate[0].re + "  到 " + choiceDate[1].re + "  共 " + dayCount +" 晚");
-			}
+			console.log(choiceDate, dayCount);
+			console.log('入住从 ' + choiceDate[0].re + '  到 ' + choiceDate[1].re + '  共 ' + dayCount + ' 晚');
 		}
 	}
+};
 </script>
 
 <style>
-	view {
-		display: flex;
-	}
+view {
+	display: flex;
+}
 
-	.content {
-		justify-content: center;
-		flex-direction: column;
-	}
+.content {
+	justify-content: center;
+	flex-direction: column;
+}
 
-	.sliderBox {
-		justify-content: center;
-		margin-right: 50upx;
-	}
+.sliderBox {
+	justify-content: center;
+	margin-right: 50upx;
+}
 
-	.text-center {
-		justify-content: center;
-	}
+.text-center {
+	justify-content: center;
+}
 
-	.rowBox {
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-	}
+.rowBox {
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+}
 
-	.mrg50T {
-		margin-top: 50upx;
-	}
+.mrg50T {
+	margin-top: 50upx;
+}
 
-	.tips {
-		color: #999;
-		font-size: 24upx;
-		text-align: center;
-		margin-top: 100upx;
-	}
+.tips {
+	color: #999;
+	font-size: 24upx;
+	text-align: center;
+	margin-top: 100upx;
+}
 </style>
